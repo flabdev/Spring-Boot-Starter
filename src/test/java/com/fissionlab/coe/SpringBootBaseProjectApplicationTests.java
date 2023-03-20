8c7a6d813fed9e364ce789949feb4e730fa71360
satyasivaprasad <satyasivaprasad.j@gmail.com>
2023-03-20 16:12:10 +0530

    Disabling docker test

--- src/test/java/com/fissionlab/coe/SpringBootBaseProjectApplicationTests.java
+++ src/test/java/com/fissionlab/coe/SpringBootBaseProjectApplicationTests.java
@@ -1,8 +1,10 @@
 package com.fissionlab.coe;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+@Disabled
 
 @SpringBootTest
 @TestPropertySource(properties = {
